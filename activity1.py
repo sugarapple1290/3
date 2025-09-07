90293f8e343adf276c0103e4670bf65de77be790
sugarapple1290 <zeeshanhadiya4@gmail.com>
2025-09-07 01:07:08 -0700

    2

---
+++ activity1.py
@@ -0,0 +1,25 @@
+#Revision lesson 
+print('hadiya')
+
+# printing a statment using a variable
+a= 'hadiya'
+print(a)
+
+#data types in python
+# string
+x='34'
+print(type(x))
+
+# float
+x=3.4
+print(type(x))
+
+#integer
+x=4567
+print(type(x))
+
+#bool
+x=True 
+print(type(x))
+
+#
---
+++ activity2.py
@@ -0,0 +1,23 @@
+#typecastingint to float
+a=5
+print(a)
+b=float(a)
+print(type(b))
+
+#typecasting from float to int
+a=3.7
+b= int(a)
+print(b)
+print(type(b))
+
+#typecasting from int to str
+
+a=8
+b=str(a)
+print(b)
+print(type(b))
+
+# typecasting from float to str
+a=8.7
+b=str(a)
+print(type(b))
